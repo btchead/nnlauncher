3b1e66f450328e887a45845d1ea7f5a06bce62d0
PolyFlower <btchead@protonmail.ch>
2023-02-11 04:04:40 +0300

    fix: reference rename done

--- NoName/MessageFlagDelegateMapper.cs
+++ NoName/MessageFlagDelegateMapper.cs
@@ -2,15 +2,15 @@
 {
 	public static void SetupMessageDelegates()
 	{
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_AUTH_STATUS, new GClass9.GDelegate0(GClass6.smethod_1));
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_KEY, new GClass9.GDelegate0(GClass6.smethod_0));
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_BROADCAST, new GClass9.GDelegate0(GClass6.smethod_3));
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_WARDEN_UPLOAD_RSP, new GClass9.GDelegate0(GClass6.smethod_2));
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_OFFSETS, new GClass9.GDelegate0(GClass6.smethod_7));
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_APAYLOAD, new GClass9.GDelegate0(GClass6.smethod_4));
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_REQUES_GAMEMODULE, new GClass9.GDelegate0(GClass6.smethod_9));
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_NEEDLE_PAYLOAD, new GClass9.GDelegate0(GClass6.smethod_5));
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_HOOK_PAYLOAD, new GClass9.GDelegate0(GClass6.smethod_6));
-		GClass9.smethod_0(ClientServerMessageFlags.SMSG_TOOL_OFFSETS, new GClass9.GDelegate0(GClass6.smethod_8));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_AUTH_STATUS, new GClass9.MessageHandlerDelegate(GClass6.smethod_1));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_KEY, new GClass9.MessageHandlerDelegate(GClass6.smethod_0));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_BROADCAST, new GClass9.MessageHandlerDelegate(GClass6.smethod_3));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_WARDEN_UPLOAD_RSP, new GClass9.MessageHandlerDelegate(GClass6.smethod_2));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_OFFSETS, new GClass9.MessageHandlerDelegate(GClass6.smethod_7));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_APAYLOAD, new GClass9.MessageHandlerDelegate(GClass6.smethod_4));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_REQUES_GAMEMODULE, new GClass9.MessageHandlerDelegate(GClass6.smethod_9));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_NEEDLE_PAYLOAD, new GClass9.MessageHandlerDelegate(GClass6.smethod_5));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_HOOK_PAYLOAD, new GClass9.MessageHandlerDelegate(GClass6.smethod_6));
+		GClass9.smethod_0(ClientServerMessageFlags.SMSG_TOOL_OFFSETS, new GClass9.MessageHandlerDelegate(GClass6.smethod_8));
 	}
 }
