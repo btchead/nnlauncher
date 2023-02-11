8ffa4a11aa9957ecd730596ff9b96af2b5864b54
PolyFlower <btchead@protonmail.ch>
2023-02-11 19:17:27 +0300

    Remove dnSpy decompiler comments
    Remove the automatically generated comments in the decompiled code to clean up the code and make it easier to read and maintain.

--- NoName/GClass6.cs
+++ NoName/GClass6.cs
@@ -2,16 +2,13 @@
 using System.Collections.Generic;
 using System.Threading;
 
-// Token: 0x0200000F RID: 15
 public class GClass6
 {
-	// Token: 0x060000B7 RID: 183 RVA: 0x00002571 File Offset: 0x00000771
 	public static void smethod_0(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		binaryReaderWrapper.ReadBytes(8);
 	}
 
-	// Token: 0x060000B8 RID: 184 RVA: 0x0000431C File Offset: 0x0000251C
 	public static void smethod_1(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		server.ushort_1 = binaryReaderWrapper.ReadUInt16();
@@ -19,7 +16,6 @@ public class GClass6
 		new Thread(new ParameterizedThreadStart(server.WaitForWorldOfWarcraft.Invoke)).Start(server.ushort_1);
 	}
 
-	// Token: 0x060000B9 RID: 185 RVA: 0x0000436C File Offset: 0x0000256C
 	public static void smethod_2(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		ConsoleColor consoleColor = (ConsoleColor)binaryReaderWrapper.ReadByte();
@@ -31,7 +27,6 @@ public class GClass6
 		Console.ForegroundColor = foregroundColor;
 	}
 
-	// Token: 0x060000BA RID: 186 RVA: 0x0000436C File Offset: 0x0000256C
 	public static void smethod_3(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		ConsoleColor consoleColor = (ConsoleColor)binaryReaderWrapper.ReadByte();
@@ -43,7 +38,6 @@ public class GClass6
 		Console.ForegroundColor = foregroundColor;
 	}
 
-	// Token: 0x060000BB RID: 187 RVA: 0x000043A8 File Offset: 0x000025A8
 	public static void smethod_4(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		int num = binaryReaderWrapper.ReadInt32();
@@ -56,7 +50,6 @@ public class GClass6
 		}
 	}
 
-	// Token: 0x060000BC RID: 188 RVA: 0x000043F0 File Offset: 0x000025F0
 	public static void smethod_5(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		int num = binaryReaderWrapper.ReadInt32();
@@ -68,7 +61,6 @@ public class GClass6
 		}
 	}
 
-	// Token: 0x060000BD RID: 189 RVA: 0x00004428 File Offset: 0x00002628
 	public static void smethod_6(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		int num = binaryReaderWrapper.ReadInt32();
@@ -80,7 +72,6 @@ public class GClass6
 		}
 	}
 
-	// Token: 0x060000BE RID: 190 RVA: 0x00004460 File Offset: 0x00002660
 	public static void smethod_7(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		uint num = binaryReaderWrapper.ReadUInt32();
@@ -97,7 +88,6 @@ public class GClass6
 		}
 	}
 
-	// Token: 0x060000BF RID: 191 RVA: 0x000044B4 File Offset: 0x000026B4
 	public static void smethod_8(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		uint num = binaryReaderWrapper.ReadUInt32();
@@ -114,19 +104,16 @@ public class GClass6
 		}
 	}
 
-	// Token: 0x060000C0 RID: 192 RVA: 0x0000257B File Offset: 0x0000077B
 	public static void smethod_9(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		binaryReaderWrapper.ReadUInt32();
 		server.action_6();
 	}
 
-	// Token: 0x060000C1 RID: 193 RVA: 0x0000213D File Offset: 0x0000033D
 	public static void smethod_10(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 	}
 
-	// Token: 0x060000C2 RID: 194 RVA: 0x00004508 File Offset: 0x00002708
 	public static void smethod_11(BinaryMessageReader binaryReaderWrapper, Server server)
 	{
 		uint num = binaryReaderWrapper.ReadUInt32();
