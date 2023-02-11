541e2989cc0bae45bf9575adc10c82985b41ec2f
PolyFlower <btchead@protonmail.ch>
2023-02-11 21:11:03 +0300

    Refactor: GClass9 & MessageFlagDelegates renamed * GClass9 now called MessageProcessor * MessageFlagDelegates now called MessageHandlerSetup * Methods refactored

---
+++ NoName/MessageHandlerSetup.cs
@@ -0,0 +1,17 @@
+﻿public class MessageHandlerSetup
+{
+	public static void SetupMessageHandlers()
+	{
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_AUTH_STATUS, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_1));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_KEY, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_0));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_BROADCAST, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_3));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_WARDEN_UPLOAD_RSP, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_2));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_OFFSETS, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_7));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_APAYLOAD, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_4));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_REQUES_GAMEMODULE, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_9));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_NEEDLE_PAYLOAD, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_5));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_HOOK_PAYLOAD, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_6));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_TOOL_OFFSETS, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_8));
+	}
+}
+	
---
+++ NoName/MessageProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MessageProcessor
+{
+	public static void RegisterMessageHandler(ClientServerMessageFlags messageFlag, MessageHandlerDelegate messageHandlerDelegate)
+	{
+		_messageHandlers[messageFlag] = messageHandlerDelegate;
+	}
+
+	public static void ProcessMessage(BinaryMessageReader binaryMessageReader, Server server, ClientServerMessageFlags messageFlag)
+	{
+		try
+		{
+			if (_messageHandlers.ContainsKey(messageFlag))
+			{
+				_messageHandlers[messageFlag](binaryMessageReader, server);
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.ToString());
+		}
+	}
+
+	public static Dictionary<ClientServerMessageFlags, MessageHandlerDelegate> _messageHandlers = new Dictionary<ClientServerMessageFlags, MessageHandlerDelegate>();
+
+	public delegate void MessageHandlerDelegate(BinaryMessageReader packet, Server server);
+}
