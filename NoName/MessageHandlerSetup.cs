53cdc4c188e28acaf355fe80792270dd42830bb6
PolyFlower <btchead@protonmail.ch>
2023-02-12 00:10:20 +0300

    related to 38526e6a

--- NoName/MessageHandlerSetup.cs
+++ NoName/MessageHandlerSetup.cs
@@ -2,16 +2,16 @@
 {
 	public static void SetupMessageHandlers()
 	{
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_AUTH_STATUS, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_1));
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_KEY, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_0));
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_BROADCAST, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_3));
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_WARDEN_UPLOAD_RSP, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_2));
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_OFFSETS, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_7));
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_APAYLOAD, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_4));
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_REQUES_GAMEMODULE, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_9));
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_NEEDLE_PAYLOAD, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_5));
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_HOOK_PAYLOAD, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_6));
-		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_TOOL_OFFSETS, new MessageProcessor.MessageHandlerDelegate(GClass6.smethod_8));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_AUTH_STATUS, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerAuthStatusMessage));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_KEY, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerKeyMessage));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_BROADCAST, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerBroadcastMessage));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_WARDEN_UPLOAD_RSP, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerWardenUploadMessage));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_OFFSETS, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerOffsetsMessage));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_APAYLOAD, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerPayloadMessage));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_REQUES_GAMEMODULE, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerRequestGameModuleMessage));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_NEEDLE_PAYLOAD, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerNeedlePayloadMessage));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_HOOK_PAYLOAD, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerHookPayloadMessage));
+		MessageProcessor.RegisterMessageHandler(ClientServerMessageFlags.SMSG_TOOL_OFFSETS, new MessageProcessor.MessageHandlerDelegate(ServerMessageHandler.HandleServerToolOffsetsMessage));
 	}
 }
 	
