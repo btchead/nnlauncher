2688eea659f07634d15aa19973c8e46da446a5a3
PolyFlower <btchead@protonmail.ch>
2023-02-11 03:43:43 +0300

    refactor: complete class refactor * renamed class, methods * methods optimized & simplified

---
+++ NoName/BinaryMessageReader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+public class BinaryMessageReader
+{
+    public ClientServerMessageFlags ClientServerMessageFlag => (ClientServerMessageFlags)messageHeaderStruct.messageFlag;
+
+    public BinaryMessageReader(byte[] messageFlagBytes, byte[] messageDataBytes)
+    {
+        binaryReader = new BinaryReader(new MemoryStream(messageDataBytes));
+
+        ushort messageFlagValue = messageFlagBytes[0];
+        uint messageLength = BitConverter.ToUInt32(messageFlagBytes, 0) & 0x00FFFFFF;
+
+        messageHeaderStruct = new MessageHeaderStruct
+        {
+            messageFlag = messageFlagValue,
+            messageLength = messageLength - 4
+        };
+
+        byte[] messageBytes = ReadBytes((int)messageHeaderStruct.messageLength);
+        binaryReader = new BinaryReader(new MemoryStream(messageBytes));
+    }
+
+    public string ReadString(int stringLength)
+    {
+        byte[] byteArray = ReadBytes(stringLength * 2 + 2);
+        byte[] cleanedByteArray = new byte[stringLength * 2 + 2];
+        for (int i = 0; i < stringLength * 2; i++)
+        {
+            if (byteArray[i] != 0)
+            {
+                cleanedByteArray[i] = byteArray[i];
+            }
+        }
+        return Encoding.Unicode.GetString(cleanedByteArray).Split(new char[1])[0];
+    }
+
+    public int ReadInt32() => binaryReader.ReadInt32();
+
+    public byte ReadByte() => binaryReader.ReadByte();
+
+    public ushort ReadUInt16() => binaryReader.ReadUInt16();
+
+    public uint ReadUInt32() => binaryReader.ReadUInt32();
+
+    public ulong ReadUInt64() => binaryReader.ReadUInt64();
+
+    public byte[] ReadBytes(int count) => binaryReader.ReadBytes(count);
+
+    private MessageHeaderStruct messageHeaderStruct;
+
+    private readonly BinaryReader binaryReader;
+}
