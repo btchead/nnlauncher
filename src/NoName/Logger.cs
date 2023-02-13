782ca10c6ac28a334c36a7d2c904f92ec37dfb2d
PolyFlower <btchead@protonmail.ch>
2023-02-13 08:31:20 +0300

    fix: auto-created namespace removed

--- src/NoName/Logger.cs
+++ src/NoName/Logger.cs
@@ -1,21 +1,25 @@
 ﻿using Serilog;
 using System;
 
-namespace NoName
+public static class Logger
 {
-    public static class Logger
+    private static readonly ILogger _logger = new LoggerConfiguration()
+        .WriteTo.Debug().CreateLogger();
+
+    public static void Info(string message)
     {
-        private static readonly ILogger _logger = new LoggerConfiguration()
-            .WriteTo.Debug().CreateLogger();
+        _logger.Information(message);
+    }
 
-        public static void Info(string message)
-        {
-            _logger.Information(message);
-        }
-
-        public static void Error(string message, Exception exception)
+    public static void Error(string message, Exception exception = null)
+    {
+        if (exception != null)
         {
             _logger.Error(message);
+        }
+        else
+        {
+            _logger.Error(exception.Message, message);
         }
     }
 }
